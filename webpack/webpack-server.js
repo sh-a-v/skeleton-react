84bcbf238ec01812e48434a331a239f0a69548ce
Shershnev <shershnev.av@gmail.com>
2015-05-30 23:33:06 +0300

    Webpack server

--- webpack/webpack-server.js
+++ webpack/webpack-server.js
@@ -1,1 +1,34 @@
+var webpack          = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
 
+var serverConfig  = require('../server/server-config');
+var webpackConfig = require('./webpack-config');
+
+module.exports = {
+  run: function() {
+    var wpConfig = webpackConfig.getConfig(options);
+    var compiler = webpack(wpConfig);
+
+    var server = new WebpackDevServer(compiler, {
+      contentBase: 'http://localhost:' + serverConfig.expressPort,
+      publicPath: 'http://localhost:' + serverConfig.webpackPort + serverConfig.assetsPath,
+
+      historyApiFallback: true,
+      hot: true,
+      delay: 50,
+      stats: {
+        colors: true
+      }
+    });
+
+    server.listen(serverConfig.webpackPort, 'localhost', function(err) {
+      setTimeout(function() {
+        console.log('Open http://localhost:' + serverConfig.expressPort);
+      }, 4000);
+
+      if (err) {
+        console.log('error:', err);
+      }
+    });
+  }
+};
